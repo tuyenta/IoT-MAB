7bd0126cd7fa19d0559f2bcd9cc3ec42373d8b66
tuyen.ta <tuyend86@gmail.com>
2019-03-01 01:44:09 +0100

    update

--- lora/utils.py
+++ lora/utils.py
@@ -1,12 +1,12 @@
 """ LPWAN Simulator: Hepper functions
-============================================
-Utilities (:mod:`lora.utils`)
-============================================
-.. autosummary::
-   :toctree: generated/
-   print_params             -- Print the arguments of the simulation.
-   sim                      -- Run the simulation
-"""    
+    ============================================
+    Utilities (:mod:`lora.utils`)
+    ============================================
+    .. autosummary::
+    :toctree: generated/
+    print_params             -- Print the arguments of the simulation.
+    sim                      -- Run the simulation
+    """
 import os
 import numpy as np
 from os.path import join, exists
@@ -24,7 +24,7 @@ def print_params(nrNodes, nrIntNodes, nrBS, radius, avgSendTime, horTime, packet
     # print parametters
     print("Simulation Parameters:")
     print ("\t Nodes:",nrNodes)
-    print ("\t Intelligent Nodes:",nrIntNodes)   
+    print ("\t Intelligent Nodes:",nrIntNodes)
     print("\t BS:",nrBS)
     print("\t Radius:", radius)
     print ("\t AvgSendTime (exp. distributed):",avgSendTime)
@@ -36,12 +36,12 @@ def print_params(nrNodes, nrIntNodes, nrBS, radius, avgSendTime, horTime, packet
     print ("\t Capture Effect:",captureEffect)
     print ("\t Inter-SF Interference:",interSFInterference)
     print ("\t Information mode:",info_mode)
-        
-def sim(nrNodes, nrIntNodes, nrBS, radius, avgSendTime, horTime, packetLength, sfSet, freqSet, 
+
+def sim(nrNodes, nrIntNodes, nrBS, radius, avgSendTime, horTime, packetLength, sfSet, freqSet,
         powSet, captureEffect, interSFInterference, info_mode, logdir, exp_name) :
     
     simtime = horTime * avgSendTime # simulation time in ms
-
+    
     grid = [10000, 10000] # maximum simulation area in m
     
     # Node parameters
@@ -50,11 +50,11 @@ def sim(nrNodes, nrIntNodes, nrBS, radius, avgSendTime, horTime, packetLength, s
     
     # phy parameters (rdd, packetLength, preambleLength, syncLength, headerEnable, crc)
     phyParams = (1, packetLength, 8, 4.25, False, True)
-
+    
     # Environement parameters (Log-shadowing model)
     logDistParams=(2.08, 107.41, 40.0) # (gamma, Lpld0, d0)
     interferenceThreshold = -150 # dBm
-
+    
     # BS parameters
     nDemodulator = 8 # number of demodulators on base-station
     
@@ -66,47 +66,47 @@ def sim(nrNodes, nrIntNodes, nrBS, radius, avgSendTime, horTime, packetLength, s
     sf11 = np.array([11,-134.5,-131.5,-128.5])
     sf12 = np.array([12,-137.0,-134.0,-131.0])
     sensi = np.array([sf7,sf8,sf9,sf10,sf11,sf12]) # array of sensitivity values
-
+    
     # The spreading factor interaction matrix derived from lab tests and Semtech documentation
     # SFs are not perfectly orthogonal: a signal at SF_m faces interferences from all signals on other SFs
     if captureEffect == True:
         captureThreshold = dBmTomW(6) # threshold is 6 dB
         if interSFInterference == True:
             interactionMatrix = np.array([
-                [dBmTomW(6), dBmTomW(-7), dBmTomW(-7), dBmTomW(-7), dBmTomW(-7), dBmTomW(-7)],
-                [dBmTomW(-10), dBmTomW(6), dBmTomW(-10), dBmTomW(-10), dBmTomW(-10), dBmTomW(-10)],
-                [dBmTomW(-12.5), dBmTomW(-12.5), dBmTomW(6), dBmTomW(-12.5), dBmTomW(-12.5), dBmTomW(-12.5)],
-                [dBmTomW(-15), dBmTomW(-15), dBmTomW(-15), dBmTomW(6), dBmTomW(-15), dBmTomW(-15)],
-                [dBmTomW(-17.5), dBmTomW(-17.5), dBmTomW(-17.5), dBmTomW(-17.5), dBmTomW(6), dBmTomW(-17.5)],
-                [dBmTomW(-20), dBmTomW(-20), dBmTomW(-20), dBmTomW(-20), dBmTomW(-20), dBmTomW(6)]])
+                                          [dBmTomW(6), dBmTomW(-7), dBmTomW(-7), dBmTomW(-7), dBmTomW(-7), dBmTomW(-7)],
+                                          [dBmTomW(-10), dBmTomW(6), dBmTomW(-10), dBmTomW(-10), dBmTomW(-10), dBmTomW(-10)],
+                                          [dBmTomW(-12.5), dBmTomW(-12.5), dBmTomW(6), dBmTomW(-12.5), dBmTomW(-12.5), dBmTomW(-12.5)],
+                                          [dBmTomW(-15), dBmTomW(-15), dBmTomW(-15), dBmTomW(6), dBmTomW(-15), dBmTomW(-15)],
+                                          [dBmTomW(-17.5), dBmTomW(-17.5), dBmTomW(-17.5), dBmTomW(-17.5), dBmTomW(6), dBmTomW(-17.5)],
+                                          [dBmTomW(-20), dBmTomW(-20), dBmTomW(-20), dBmTomW(-20), dBmTomW(-20), dBmTomW(6)]])
         else:
             interactionMatrix = np.array([
-                [dBmTomW(6), 0,          0,             0,          0,          0         ],
-                [0,          dBmTomW(6), 0,             0,          0,          0         ],
-                [0,          0         , dBmTomW(6),    0,          0,          0         ],
-                [0,          0         , 0,             dBmTomW(6), 0,          0         ],
-                [0,          0         , 0,             0,          dBmTomW(6), 0         ],
-                [0,          0         , 0,             0,          0,          dBmTomW(6)]])
-    else:
-        captureThreshold = 0 # threshold is 0
+                                          [dBmTomW(6), 0,          0,             0,          0,          0         ],
+                                          [0,          dBmTomW(6), 0,             0,          0,          0         ],
+                                          [0,          0         , dBmTomW(6),    0,          0,          0         ],
+                                          [0,          0         , 0,             dBmTomW(6), 0,          0         ],
+                                          [0,          0         , 0,             0,          dBmTomW(6), 0         ],
+                                          [0,          0         , 0,             0,          0,          dBmTomW(6)]])
+else:
+    captureThreshold = 0 # threshold is 0
         interactionMatrix = np.eye(len(sfSet), dtype=int)
-    
+
     # Location generator
     print ("======= Generate/Load simulation scenario =======")
     distMatrix, bestDist, bestSF, bestBW = getMaxTransmitDistance(sensi, maxPtx, logDistParams, phyParams)
     print ("Max range = {} at SF = {}, BW = {}".format(bestDist, bestSF, bestBW))
 
-    # Generate base station and nodes
-    np.random.seed(12345) # seed the random generator
-    
+# Generate base station and nodes
+np.random.seed(12345) # seed the random generator
+
     # Place base-stations randomly
     simu_dir = join('logs', exp_name)
     #make folder
     if not exists(simu_dir):
         makedirs(simu_dir)
-    file_1 = join(simu_dir, "bsList_bs" + str(nrBS) + "_nodes" + str(nrNodes) + ".npy")
-    file_2 = join(simu_dir, "nodeList_bs"+ str(nrBS) + "_nodes" + str(nrNodes) + ".npy")
-    
+file_1 = join(simu_dir, "bsList_bs" + str(nrBS) + "_nodes" + str(nrNodes) + ".npy")
+file_2 = join(simu_dir, "nodeList_bs"+ str(nrBS) + "_nodes" + str(nrNodes) + ".npy")
+
     if not os.path.exists(file_1):
         if not os.path.exists(file_2):
             print ("Generated locations for {} base-stations and {} nodes".format(nrBS, nrNodes))
@@ -123,16 +123,16 @@ def sim(nrNodes, nrIntNodes, nrBS, radius, avgSendTime, horTime, packetLength, s
             # save to file
             np.save(file_1, BSLoc)
             np.save(file_2, nodeLoc)
-    else:
-        # Load =location
-        print ("\t Load locations for {} base-stations and {} nodes".format(nrBS, nrNodes))
-        BSLoc = np.load(file_1)
-        nodeLoc = np.load(file_2)
+else:
+    # Load =location
+    print ("\t Load locations for {} base-stations and {} nodes".format(nrBS, nrNodes))
+    BSLoc = np.load(file_1)
+    nodeLoc = np.load(file_2)
     
     # Simulation
     nTransmitted = 0
     nRecvd = 0
-
+    
     BSList = BSLoc[0:nrBS,:]
     nodeList = nodeLoc[0:nrNodes,:]
     nodeList[0:nrIntNodes, -1] = 1 # Intelligent nodes
@@ -140,71 +140,62 @@ def sim(nrNodes, nrIntNodes, nrBS, radius, avgSendTime, horTime, packetLength, s
     print ("# base-stations = {}".format(nrBS))
     print ("# nodes = {}".format(nrNodes))
     #fname = 'prob_'
-
+    
     env = simpy.Environment()
     env.process(cuckooClock(env))
-
+    
     fname = 'prob_'+ str(nrIntNodes) +'_smartNodes_' + 'infoMode_' + str(info_mode) + '_captureEffect_' + str(captureEffect) + '_interSFMode_' + str(interSFInterference)
-
-    bsDict = {} # setup empty dictionary for base-stations  
+    
+    bsDict = {} # setup empty dictionary for base-stations
     for elem in BSList:
         bsDict[int(elem[0])] = myBS(int(elem[0]), (elem[1], elem[2]), interactionMatrix, nDemodulator, ackLength, freqSet, sfSet, captureThreshold)
-        
+
     nodeDict = {} # setup empty dictionary for nodes
     for elem in nodeList:
         node = myNode(int(elem[0]), (elem[1], elem[2]), elem[3:13], sfSet, freqSet, powSet, BSList, interferenceThreshold, logDistParams,
                       sensi, elem[13], info_mode, horTime)
-        nodeDict[node.nodeid] = node
-        env.process(transmitPacket(env, node, bsDict, logDistParams))
-    
-    # compute traffic
-    env.process(saveProb(env, nodeDict, fname, simu_dir))
-    env.run(until=simtime)
-    
+                      nodeDict[node.nodeid] = node
+                      env.process(transmitPacket(env, node, bsDict, logDistParams))
+
+# compute traffic
+env.process(saveProb(env, nodeDict, fname, simu_dir))
+env.run(until=simtime)
+
     # reception
     for nodeid, node in nodeDict.items():
         nTransmitted += node.packetsTransmitted
         nRecvd += node.packetsSuccessful
-    
-    # print results        
-    print ("================== Results ==================")
-    print ("# Transmitted = {}".format(nTransmitted))
-    print ("# Received = {}".format(nRecvd))
-    print ("# Ratio = {}".format(nRecvd/nTransmitted))
 
+# print results
+print ("================== Results ==================")
+print ("# Transmitted = {}".format(nTransmitted))
+print ("# Received = {}".format(nRecvd))
+print ("# Ratio = {}".format(nRecvd/nTransmitted))
+
+    # Plotting - location
+    #plotLocations(BSLoc, nodeLoc, grid[0], grid[1], bestDist, distMatrix)
+    
     # save and plot data
     probDict = {}
     for nodeid in range(nrIntNodes):
         filename = join(simu_dir, str(fname) + '_id_' + str(nodeid) + '.csv')
         df = pd.read_csv(filename, delimiter=' ', header= None, index_col=False)
-        df = df.replace([',','}'], ['',''], regex=True)
-            
-        drop_list = []
-        for i in range(df.shape[1]):
-            if (i%2 == 0):
-                drop_list.append(i)
-        df = df.drop(drop_list, axis=1).astype('float64').values
+        df = df.replace(',', '', regex=True).astype('float64').values
         
         fig, ax = plt.subplots(figsize=(8,5))
         for idx in range(df.shape[1]):
             ax.plot(df[:,idx], label = 'SF = {}'.format(12-df.shape[1]+idx+1))
             plt.xlabel("Horizon time")
             plt.ylabel("Probability")
-            ax.legend(loc='best')   
+            ax.legend(loc='best')
             plt.rcParams["font.family"] = "sans-serif"
             plt.rcParams["font.size"] = 16
             plt.close(fig)
-        
         # save to file
         fig.savefig(join(simu_dir, str(fname) + '_id_' + str(nodeid) + '.png'))
-        
         # probDict
         probDict[nodeid] = df[-1 , :]
-
-    # save probDict        
+    # save probDict
     np.save(join(simu_dir, str(fname)), probDict)
 
-    # Plotting - location
-    plotLocations(BSLoc, nodeLoc, grid[0], grid[1], bestDist, distMatrix)
-    
     return(bsDict, nodeDict)
